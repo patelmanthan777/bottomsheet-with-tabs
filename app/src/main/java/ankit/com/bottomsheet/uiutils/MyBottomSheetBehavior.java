43d196fbc85fa534cd9bf0ebb0aa4f3fb3abd7d6
patelmanthan777 <patelmanthan777@gmail.com>
2019-07-16 18:22:27 +0530

    Revert "In some devices mViewDragHelper getting null"

--- app/src/main/java/ankit/com/bottomsheet/uiutils/MyBottomSheetBehavior.java
+++ app/src/main/java/ankit/com/bottomsheet/uiutils/MyBottomSheetBehavior.java
@@ -296,9 +296,6 @@ public class MyBottomSheetBehavior<V extends View> extends CoordinatorLayout.Beh
 
     @Override
     public boolean onTouchEvent(CoordinatorLayout parent, V child, MotionEvent event) {
-         if (mViewDragHelper == null) {
-            mViewDragHelper = ViewDragHelper.create(parent, mDragCallback);
-        }
         if (!child.isShown()) {
             return false;
         }
